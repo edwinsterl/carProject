cd7c7bd296a9fce0cfab2888ccf8ae4305cf4493
edwinsterl <you@example.com>
2021-11-04 23:40:00 -0400

    Merge branch 'main' of C:\Users\edwin\eclipse-workspace\gitStashExamples with conflicts.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,9 @@ public class Main {
         Car newcar= new Car();
         newcar.name="honda";
         newcar.color="red";
+        newcar.doors= 3;
         newcar.engineCapacity ="5651210";
+        System.out.println(newcar.color);
         
     }
 
