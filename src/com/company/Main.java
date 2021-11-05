15b694233516dddf948d78b922117db4aa28001e
edwinsterl <you@example.com>
2021-11-04 22:46:52 -0400

    the basic structure of the car

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Car newcar= new Car();
+        newcar.name="honda";
+        newcar.color="red";
+        newcar.engineCapacity ="5651210";
+        
+    }
+
+
+
+
+
+}
